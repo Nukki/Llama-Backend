f057ab9e8988d02cb0eb14d7e5b0704ae0ee1162
Nukki <nikki6052096@gmail.com>
2019-01-20 19:08:27 -0500

    add person and responders. works-ish

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,6 @@ const express = require('express');
 const router = express.Router();
 const utils = require('../utils');
 
-// active person sends background location updates here
-router.post('/update', (req, res) => {
- utils.updateLocation({ // save location to db
-   uuid: req.body.uuid,
-   lat: req.body.lat,
-   long: req.body.long,
- })
-})
-
 // starts a socket room for geofencing and location sharing
 router.post('/notify', (req, res) => {
   const apn = req.apn;
@@ -18,11 +9,17 @@ router.post('/notify', (req, res) => {
   const io = req.io;
 
   console.log(`user ${req.body.uuid} needs help. coords: ${req.body.lat} ${req.body.long}`);
-  const roomName = utils.createRoom({
-    uuid: req.body.uuid,
-    lat: req.body.lat,
-    long: req.body.long
-  });
+
+  // update location
+  // update safe status
+  const llama = { long: req.body.long, lat: req.body.lat, uuid: req.body.uuid };
+  utils.updateLocation(llama);
+  utils.setSafeStatus(llama, false);
+  // const roomName = utils.createRoom({
+  //   uuid: req.body.uuid,
+  //   lat: req.body.lat,
+  //   long: req.body.long
+  // });
   // broadcast to all connections about new room
   io.sockets.emit('new_llama');
 
@@ -51,29 +48,40 @@ router.post('/notify', (req, res) => {
   res.status(200).send('we were notified!');
 })
 
-// unsafe person sends location updates here
-router.post('/llamaUpdate', (req, res) => {
- // save new location to db
- utils.updateRoom(req.body.uuid, req.body.lat, req.body.long);
- const io = req.io;
- // const arr = [ { name: req.body.name, long: req.body.long, lat: req.body.lat, uuid: req.body.uuid},];
- const llama = { long: req.body.long, lat: req.body.lat, uuid: req.body.uuid };
- io.sockets.in(req.body.uuid).emit('update_llama', llama);
- console.log('emitted unsafe person location update');
- res.status(200).send('unsafe update success');
-})
-
 // closes the socket room with location sharing
 router.post('/imsafe', (req, res) => {
   console.log('im safe');
-  utils.removeRoom(req.body.uuid);
+  const llama = { long: req.body.long, lat: req.body.lat, uuid: req.body.uuid };
+  utils.setSafeStatus(llama, true);
   const io = req.io;
+  // disappear from other people's radars
   io.sockets.in(req.body.uuid).emit('clear', req.body.uuid);
   io.of('/').in(req.body.uuid).clients((err, clients) => {
     console.log(clients);
     clients.forEach((c) => io.sockets.connected[c].leave(req.body.uuid));
   });
+  // leave all responder rooms, but not llamas 
   res.send('You are safe! awesome!')
+})
+
+// active person sends background location updates here
+router.post('/update', (req, res) => {
+ utils.updateLocation({ // save location to db
+   uuid: req.body.uuid,
+   lat: req.body.lat,
+   long: req.body.long,
+ })
+ // update responder ?
+})
+
+// unsafe person sends background location updates here
+router.post('/llamaUpdate', (req, res) => {
+ const llama = { long: req.body.long, lat: req.body.lat, uuid: req.body.uuid };
+ utils.updateLocation(llama); // save new location to db
+ const io = req.io;
+ io.sockets.in(req.body.uuid).emit('update_llama', llama);
+ console.log('emitted unsafe person location update');
+ res.status(200).send('unsafe update success');
 })
 
 
--- sockets/index.js
+++ sockets/index.js
@@ -1,28 +1,46 @@
 const utils = require('../utils');
 
 module.exports = (socket, io) => {
+
   socket.on('create_user', (user_object) => {
     console.log("Creating new user!")
     socket.emit('user_created', utils.createUser(user_object));
   });
+
   socket.on('active', (user_object) => {
-    console.log("inside active");
+    console.log("inside active, user is ", user_object);
     utils.setActiveStatus(user_object, true);
     utils.updateLocation(user_object);
-    utils.getRoomsToJoin(user_object, (rooms) => {
+    utils.getLlamas(user_object, (rooms) => {
       rooms.forEach((room) => {
-        if (!(room.name in socket.rooms)) {
-          socket.join(room.name);
-          console.log(`socket ${socket.id} joined room ${room.name}`);
-          const llama = { uuid: room.name, lat: `${room.lat}`, long: `${room.long}` }
+        if (!(room.uuid in socket.rooms)) {
+          socket.join(room.uuid);
+          console.log(`socket ${socket.id} joined llama room ${room.uuid}`);
+          const llama = { uuid: room.uuid, lat: `${room.lat}`, long: `${room.long}` }
           socket.emit('add_llama', llama);
         }
       })
     })
+    if (!user_object.isSafe) {
+      utils.getResponders(user_object, (responders) => {
+        responders.forEach((re) => {
+          if(!(re.uuid in socket.rooms)) {
+            socket.join(re.uuid);
+            console.log(`socket ${socket.id} joined responder room ${re.uuid}`);
+            const resp = { uuid: re.uuid, lat: `${re.lat}`, long: `${re.long}` }
+            socket.emit('add_responder', resp);
+          }
+        })
+      })
+    } // end if
   });
+
   socket.on('not_active', (user_object) => {
     utils.setActiveStatus(user_object, false);
+    // disappear from everyone's radar
+    io.sockets.in(user_object.uuid).emit('clear', user_object.uuid);
   });
+
   socket.on('disconnect', () => {
     console.log(`socket ${socket.id} disonnected`);
   })
